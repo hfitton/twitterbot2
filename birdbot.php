fd41769910672977cec5710fc321d50660c8e0b3
Helen Fitton <hfitton@live.com>
2017-01-22 14:49:48 +1300

    Add files via upload
    Added array and tweet random value and number.

--- birdbot.php
+++ birdbot.php
@@ -9,16 +9,28 @@
 require_once('codebird.php');
 //echo $test_import;
  
-\Codebird\Codebird::setConsumerKey("tHVPUVF5ZmZOHlk4J6Uv3ZVGK", "SpNy5wdChSDjUV5reBnp6e3ZN53d8caEnadXKRD5uu6wGzhfmD");
+\Codebird\Codebird::setConsumerKey("cons key", "cons key secret");
 
 $cb = \Codebird\Codebird::getInstance();
 
-$cb->setToken("819014329710186496-xOMMQ9Mt1aMbAkYiun7KDq8Lzq9ToRE", "UT6SxcVPg2MoR3LMXPFlEMbNUqW7OsLWfG792LHgbkkvs");
+$cb->setToken("access token", "token secret");
 
+$r = rand(0,100); // to ensure a diff tweet is posted each time. 
 
-$params = array(
-  'status' => 'Stuff. #php #twitter'
-);
-$reply = $cb->statuses_update($params);
+$data = array(
+		"I like white", 
+		"I like black", 
+		"My favourite colour is red", 
+		"Northerners do it better",
+		"New Zealand is the best.",
+		"Everyone wants to talk like a Yorkshireman."
+		);
+
+//this line works a treat. Try not to break it too much.  
+$reply = $cb->statuses_update("status= " . $data[array_rand($data)] . " " . $r );
+
+//http://www.techrepublic.com/article/17-useful-functions-for-manipulating-arrays-in-php/5792851/
+//crucial for helpig find the right syntax for the array.  
+
 
 ?>
